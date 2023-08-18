0af49467b6321466933da72c9b5689b5ed745862
Ridvan <rvozger@gmail.com>
2023-08-18 19:55:30 +0300

    deneme2

--- src/test/java/day05_relativeLocators_maven/C05_GithubDeneme.java
+++ src/test/java/day05_relativeLocators_maven/C05_GithubDeneme.java
@@ -3,5 +3,7 @@ package day05_relativeLocators_maven;
 public class C05_GithubDeneme {
     public static void main(String[] args) {
         System.out.println("hi github");
+        System.out.println("come on github");
+
     }
 }
