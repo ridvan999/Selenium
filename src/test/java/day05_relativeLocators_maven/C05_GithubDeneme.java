2469c5fe04a4206531647efdedb6e9e487372641
Ridvan <rvozger@gmail.com>
2023-08-18 19:57:07 +0300

    SeleniumT113

--- src/test/java/day05_relativeLocators_maven/C05_GithubDeneme.java
+++ src/test/java/day05_relativeLocators_maven/C05_GithubDeneme.java
@@ -4,6 +4,7 @@ public class C05_GithubDeneme {
     public static void main(String[] args) {
         System.out.println("hi github");
         System.out.println("come on github");
+        System.out.println("yine olmadi");
 
     }
 }
